34081b0e443f5424744e8cd51572dd34c74ec5e4
Chimanguero <alruhle@gmail.com>
2026-05-26 20:42:48 -0300

    a

--- blog/models.py
+++ blog/models.py
@@ -110,3 +110,37 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comentario de {self.author.username} en '{self.post.title}'"
+    
+    class Instructor(models.Model):
+        """Instructor del curso."""
+        nombre = models.CharField(max_length=100)
+        especialidad  = models.CharField(max_length=200, unique=True)
+        user = models.OneToOneField(
+            User,
+            on_delete=models.CASCADE,
+            related_name="perfil_instructor",
+        )
+        class Meta:
+            verbose_name = "Instructor"
+            verbose_name_plural = "Instructores"
+            ordering = ["nombre"]
+
+        def __str__(self):
+            return self.nombre
+        
+    class Alumno(models.Model):
+        """Alumno del curso."""
+        user = models.OneToOneField(
+            User,
+            on_delete=models.CASCADE,
+            related_name="perfil_alumno",
+        )
+        legajo = models.CharField(max_length=20, unique=True)
+        fecha_ingreso = models.DateField(auto_now_add=True)
+        class Meta:
+            verbose_name = "Alumno"
+            verbose_name_plural = "Alumnos"
+            ordering = ["nombre"]
+
+        def __str__(self):
+            return self.nombre
